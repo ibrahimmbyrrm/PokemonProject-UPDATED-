5e8fa432c1d77a44a9006b89055833ba12b3ada0
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-05-28 01:07:05 +0300

    pokelist tableviewcontroller created

---
+++ PokemonWorld/Controllers/PokemonListTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonListTableViewController.swift
+//  PokemonWorld
+//
+//  Created by İbrahim Bayram on 28.05.2023.
+//
+
+import UIKit
+
+class PokemonListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
